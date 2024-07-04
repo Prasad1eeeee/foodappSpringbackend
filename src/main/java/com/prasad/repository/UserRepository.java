b11a87ac071b9c33aadca65fe245aaa6535e9bc9
Prasadpbk <129621326+Prasad1eeeee@users.noreply.github.com>
2024-07-04 10:03:03 +0530

    edit

--- src/main/java/com/prasad/repository/UserRepository.java
+++ src/main/java/com/prasad/repository/UserRepository.java
@@ -3,6 +3,6 @@ package com.prasad.repository;
 import com.prasad.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Long > {
     public User findByEmail(String username);
 }
